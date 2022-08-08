2d8a674feecec349282afbc5bc6155526acba556
Denys Melnyk <germionabest26@gmail.com>
2022-08-08 22:14:54 +0300

    fix for ses

--- functions/smtp.js
+++ functions/smtp.js
@@ -3,21 +3,26 @@
 const path = require('path');
 const fs = require('fs');
 const ejs = require('ejs');
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
+let aws = require('@aws-sdk/client-ses');
+let { defaultProvider } = require('@aws-sdk/credential-provider-node');
 
+const ses = new aws.SES({
+  apiVersion: '2010-12-01',
+  region: 'eu-central-1',
+  defaultProvider,
+});
+
+// create Nodemailer SES transporter
 let transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.SMTP_USER, // generated ethereal user
-    pass: process.env.SMTP_PASS, // generated ethereal password
-  },
+  SES: { ses, aws },
 });
 
 module.exports.getTemplate = (filename, body = {}) => {
   const emailTemplatePath = path.join(__dirname, '../', 'email-templates', filename);
   const template = fs.readFileSync(emailTemplatePath, { encoding: 'utf-8' });
   return ejs.render(template, body);
-}
+};
 
 module.exports.send = async (email, subject, template) => {
   try {
@@ -26,9 +31,18 @@ module.exports.send = async (email, subject, template) => {
       to: email, // list of receivers
       subject: subject || 'Subject', // Subject line
       html: template, // html body
-    }
+      ses: {
+        // optional extra arguments for SendRawEmail
+        Tags: [
+          {
+            Name: 'tag_name',
+            Value: 'tag_value',
+          },
+        ],
+      },
+    };
     await transporter.sendMail(options);
   } catch (error) {
     console.error(error);
   }
-}
+};
